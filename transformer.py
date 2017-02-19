aab9bc4b9d410e844dfb2c6f325436e6cdbf4025
furkhat <vailodf@gmail.com>
2017-02-19 15:05:23 +0600

    change transformer base

--- transformer.py
+++ transformer.py
@@ -2,33 +2,35 @@ import string
 
 
 alphabet = string.ascii_lowercase
-number_of_letters = len(alphabet)
+base = len(alphabet) + 1
 
 def word_to_number(word):
     code = 0
     for i in range(len(word)):
         letter_code = alphabet.index(word[i]) + 1
-        code += letter_code * (number_of_letters ** i)
+        code += letter_code * (base ** i)
     return code
 
 def number_to_word(number):
     letters = []
     while number != 0:
-        position = number % number_of_letters
+        position = number % base
         letter = alphabet[position - 1]
         letters.append(letter)
-        number //= number_of_letters
+        number //= base
     return ''.join(letters)
 
 
 def test():
-    assert word_to_number('a')   == 1
-    assert word_to_number('abc') == 2081, "1 + 2*26 + 3*26^2"
-    assert word_to_number('hello') == 7073802, "8 + 5*26 + 12*26^2 + 12*26^3 + 15*26^4"
+    assert word_to_number('a')       == 1
+    assert word_to_number('abc')     == 2242, "1 + 2*27 + 3*27^2"
+    assert word_to_number('victory') == 9952160278
+    assert word_to_number('zd')      == 134
     
-    assert number_to_word(1) == 'a'
-    assert number_to_word(2081) == 'abc'
-    assert number_to_word(7943967612) == 'victory'
+    assert number_to_word(1)          == 'a'
+    assert number_to_word(2242)       == 'abc'
+    assert number_to_word(9952160278) == 'victory'
+    assert number_to_word(134)        == 'zd'
 
 if __name__ == '__main__':
     test()
